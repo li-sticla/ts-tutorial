c55771665e9499e2e5a4f56474625e3b1cda6cb9
li-sticla <yuanzichao@163.com>
2021-08-26 05:06:03 +0800

    feat: 断言

---
+++ src/assertion.ts
@@ -0,0 +1,10 @@
+/**类型断言 */
+//告诉编译器某个值确实是我们所认为的值，从而让编译器进行正确的类型推断
+const getStrLength = (target: string | number): number => {
+  if ((<string>target).length) {
+    // 这种形式在JSX代码中不可以使用，而且也是TSLint不建议的写法
+    return (target as string).length; // 这种形式是没有任何问题的写法，建议始终使用这种形式
+  } else {
+    return target.toString().length;
+  }
+};
